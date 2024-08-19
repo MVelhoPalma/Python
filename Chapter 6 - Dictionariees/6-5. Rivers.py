8db8cd60b06ef68430c7659ebc6abc5bceb352a5
Nuno Silva <nuno.silva.ext@siemens.com>
2024-08-19 19:45:27 +0100

    rm ex6

--- Chapter 6 - Dictionariees/6-5. Rivers.py
+++
@@ -1,7 +0,0 @@
-rivers = {
-    'nile': 'egypt',
-    'amazonia': 'brazil',
-    'Yangtze': 'china'
-}
-for names, countries in rivers.items():
-    print(f"the river {names} passes through {countries}!")
